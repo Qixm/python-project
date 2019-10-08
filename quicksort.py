97c79e4436f179c9b38673da475d164820506bb2
xiaomo <32915341+Qixm@users.noreply.github.com>
2019-10-08 15:16:40 +0800

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -4,12 +4,15 @@
 快速排序的速度取决于选择的基准值
 '''
 def quicksort(array):
+    #基线条件：空，或者只有一个元素的数组是‘有序’的
     if len(array) < 2:
         return array
     else:
+         #递归条件
         pivot = array[0]
+        #由所有小于基准值的元素构成的子数组
         less = [i for i in array[1:] if i <= pivot]
-
+        #由所有大于基准值的元素构成的子数组
         greater = [i for i in array[1:] if i > pivot]
         return quicksort(less) + [pivot] + quicksort(greater)
 
